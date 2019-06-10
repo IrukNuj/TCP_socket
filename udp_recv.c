111557bd65c1cb8cee48914d4c7398f107454dd6
Admin <b1716568@planet.kanazawa-it.ac.jp>
2019-06-10 16:25:15 +0900

    add: recv

--- udp_recv.c
+++ udp_recv.c
@@ -23,7 +23,23 @@ int main(int argc, char *argv[]){
     // 自分用のソケットアドレスの作成
     sa.sin_family = AF_INET;
     sa.sin_port = htons(atoi(argv[1]));
-    sa.sin_addr.s_addr = inet_addr("0.0.0.0")
+    sa.sin_addr.s_addr = inet_addr("0.0.0.0");
+
+    //ソケットアドレスをバインド
+    bind(sock, (struct sockaddr*)&sa, sizeof(struct sockaddr_in));
+    while (1){
+#if CHECK_CLIENT // 送信元を特定する場合
+        sa_len = sizeof(struct sockaddr_in);
+        len = recvfrom(sock, buff, 4096, 0, (struct sockaddr*)&sa2, &sa_len);
+        buff[len] = '\0';
+        printf("[%s:%d] %s\n", inet_ntoa(sa2.sin_addr), ntohs(sa2.sin_port), buff);
+#else //特定しない場合
+        len = recvfrom(sock, buff, 4096, 0, NULL, NULL);
+        write(fd, buff, len);
+#endif
+    }
+    close(sock);
+    return 0;
 
 
 }
